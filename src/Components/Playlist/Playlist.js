4c6c6746796086df13894cdba55d01087e5f4e4c
Jacqueline <maaaander@gmail.com>
2020-08-17 15:58:45 -0400

    created compartment files and repos

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Playlist extends React.Component {
+    render() {
+        <div class="Playlist">
+            <input value="New Playlist"/>
+             <!-- Add a TrackList component -->
+            <button class="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
+    }
+}
+
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Searchbar.css'
+
+class SearchBar extends React.Component {
+render() {
+<div className="SearchBar">
+  <input placeholder="Enter A Song, Album, or Artist" />
+  <button className="SearchButton">SEARCH</button>
+</div>
+}
+}
