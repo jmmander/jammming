842891583ea46fa24dd4c496ed6729b3c8c77246
Jacqueline <maaaander@gmail.com>
2020-08-19 17:21:31 -0400

    added more api calls

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -38,12 +38,63 @@ const Spotify = {
                         const expiresIn = urlParams.get('expires_in');
                         window.setTimeout(() => userAccessToken = '', expiresIn * 1000);
                         window.history.pushState('Access Token', null, '/');
+
                             }
                      else  {     
                         window.location.replace(`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectId}`)
                         }
-                    }
-}
+                    },
+    
+    savePlaylist(name, uris) {
+        if (name && uris) {
+            const accessToken = this.getAccessToken;
+            const header = {Authorization: `Bearer ${accessToken}`};
+            let userId;
+            const userIdEndpoint = 'https://api.spotify.com/v1/me'
+            userId = fetch(userIdEndpoint, {headers: header}).then(
+                reponse => {
+                    return response.json()}).then(
+                        jsonRepsonse => {
+                            if (jsonRepsonse.id) {
+                                return jsonRepsonse.id;
+                            }
+                        }
+                    )
+            
+            const playlistEndpoint = `https://api.spotify.com/v1/users/${userId}/playlists`;
+            const playlistHeaders = {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    Content-Type: 'application/json'
+                        },
+                body: {name: name}
+                }
+            const playlistId = fetch(playlistEndpoint, playlistHeaders).then(
+                response=> {
+                    return response.json()}).then(
+                        jsonRepsonse => {
+                            if (jsonRepsonse.id)
+                                return jsonRepsonse.id
+                        }
+                    )
+            const addTracksUri = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`
+            const addTrackHeaders = {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    Content-Type: 'application/json'
+                        },
+                body: {uris}
+
+                                    }
+                }
+                return "needs varibles"
+            
+                }
+            
+        }
+            
+    
+
 
    
 export default Spotify
