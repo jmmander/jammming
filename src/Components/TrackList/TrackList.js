484e4465306bb016e36cc4b6b10e6f51cd4baa7d
Jacqueline <maaaander@gmail.com>
2020-08-19 13:40:53 -0400

    added change name functionality

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,14 +3,18 @@ import Track from "../Track/Track"
 
 class TrackList extends React.Component {
 
+  removeTrack() {
+    this.props.track
+  }
+
 
 
     render() {
         return(
         <div className="TrackList">
           {
-          this.props.playlistTracks.map(track => {
-            return <Track track={track} key={track.id} onAdd={this.props.onAdd}/>
+          this.props.tracks.map(track => {
+            return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemoval={this.props.onRemoval} isRemoval={this.props.isRemoval}/>
           })
         }
         </div>
